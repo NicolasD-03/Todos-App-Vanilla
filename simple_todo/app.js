06674f1c575496152801552c62c69dfd5ca05f7e
DICK Nicolas <dick.nicolas57@gmail.com>
2021-08-24 00:19:13 +0200

    style: Prettier
    Change the documents formatting with prettier

--- simple_todo/app.js
+++ simple_todo/app.js
@@ -1,76 +1,72 @@
-const todos = document.querySelector('.todos');
-const textInput = document.querySelector('div input');
-const form = document.querySelector('form')
+const todos = document.querySelector(".todos");
+const textInput = document.querySelector("div input");
+const form = document.querySelector("form");
 
 let id = 0;
 
-const createTodo = function(content){
+const createTodo = function (content) {
+  id++;
 
-    id++;
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
 
-    const todo = document.createElement('div');
-    todo.classList.add('todo');
+  // Todo check state
+  const todoCheck = document.createElement("div");
+  todoCheck.classList.add("todo-check");
 
-    // Todo check state 
-    const todoCheck = document.createElement('div');
-    todoCheck.classList.add('todo-check');
-    
-    const checkBoxNodeTree = `
-    <input id="todo-${ id }" type="checkbox">
-    <label for="todo-${ id }">
+  const checkBoxNodeTree = `
+    <input id="todo-${id}" type="checkbox">
+    <label for="todo-${id}">
         <svg><use xlink:href="#check"></use></svg>
     </label>
     `;
-    todoCheck.innerHTML = checkBoxNodeTree;
+  todoCheck.innerHTML = checkBoxNodeTree;
 
-    // Todo text content
-    const todoContent = document.createElement('div');
-    todoContent.classList.add('todo-content')
-    todoContent.innerText = content;
+  // Todo text content
+  const todoContent = document.createElement("div");
+  todoContent.classList.add("todo-content");
+  todoContent.innerText = content;
 
+  // Todo clear button
+  const todoClear = document.createElement("div");
+  todoClear.classList.add("todo-clear");
 
-    // Todo clear button
-    const todoClear = document.createElement('div');
-    todoClear.classList.add('todo-clear')
-
-    const deleteNodeTree = `
+  const deleteNodeTree = `
         <svg><use xlink:href="#delete"></use></svg>
     `;
-    todoClear.innerHTML = deleteNodeTree;
+  todoClear.innerHTML = deleteNodeTree;
 
-    todo.appendChild(todoCheck);
-    todo.appendChild(todoContent);
-    todo.appendChild(todoClear);
+  todo.appendChild(todoCheck);
+  todo.appendChild(todoContent);
+  todo.appendChild(todoClear);
 
-    todo.querySelector('.todo-check').addEventListener('change', () => {
-        updateTodoState(todo);
-    });
+  todo.querySelector(".todo-check").addEventListener("change", () => {
+    updateTodoState(todo);
+  });
 
-    todo.querySelector('.todo-clear').addEventListener('click', () => {
-        deleteTodo(todo);
-    });
+  todo.querySelector(".todo-clear").addEventListener("click", () => {
+    deleteTodo(todo);
+  });
 
-    return todo;
-}
+  return todo;
+};
 
-const addTodo = function(todo){
-    todos.prepend(todo);
-}
+const addTodo = function (todo) {
+  todos.prepend(todo);
+};
 
-const updateTodoState = function(todo){
-    todo.classList.toggle('done');
-}
+const updateTodoState = function (todo) {
+  todo.classList.toggle("done");
+};
 
-const deleteTodo = function(todo){
-    todo.remove();
-}
+const deleteTodo = function (todo) {
+  todo.remove();
+};
 
-form.addEventListener('submit', (e) =>{
-    const { value } = textInput;
-    textInput.value = '';
-    const todo = createTodo(value)
-    addTodo(todo);
-    e.preventDefault();
-
-
-})
+form.addEventListener("submit", (e) => {
+  const { value } = textInput;
+  textInput.value = "";
+  const todo = createTodo(value);
+  addTodo(todo);
+  e.preventDefault();
+});
